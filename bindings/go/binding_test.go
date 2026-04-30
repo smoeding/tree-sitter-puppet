4af1d7e9e64a67dc83110ed4b94f153309e896fb
Jesse Hathaway <jesse@mbuki-mvuki.org>
2026-04-30 16:44:46 -0500

    Fix go importing
    This is attempt two, as the first attempt,
    bf66a6adebbbd3d90fe2d7135d80986ac27bf12b, only worked locally when using
    a go.mod require directive.
    
    When grabbing a module remotely, Go assumes that all dependent code is
    in the last directory, e.g.
    
      go get github.com/smoeding/tree-sitter-puppet/bindings/go
    
    Grabs everything in /go and below, so compilation fails, since the C
    code in the parent directory cannot be found.
    
    This problem does not occur in local development however, since the
    parent directory does exist, which is why adding a require directive
    masks the problem.
    
    Instead we want to use the go.mod file /tree-sitter-puppet/go.mod and
    delete tree-sitter-puppet/bindings/go.mod. Then a go get will grab the C
    code as well.
    
    This is also how other tree-sitter bindings are setup:
    
      https://github.com/tree-sitter-grammars/tree-sitter-yaml
    
    Two other changes are made:
    
    1. Switch the Go tree-sitter bindings from
       github.com/smacker/go-tree-sitter to
       github.com/tree-sitter/tree-sitter-puppet, as the latter are
       unmaintained.
    2. Add an indirect dep on github.com/mattn/go-pointer

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,8 +3,8 @@ package tree_sitter_puppet_test
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/tree-sitter/tree-sitter-puppet"
+	tree_sitter_puppet "github.com/smoeding/tree-sitter-puppet/bindings/go"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
